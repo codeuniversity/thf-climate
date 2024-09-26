8fe03774fc3c90a2d405c244042786a6d82a9b6d
tomADC443 <61840572+tomADC443@users.noreply.github.com>
2024-09-26 11:38:44 +0200

    feat add sys path and call function

--- thf_climate/gee/auth.py
+++ thf_climate/gee/auth.py
@@ -1,3 +1,9 @@
+import os
+import sys
+
+# Add the project root to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+
 import ee
 
 from thf_climate import config
@@ -7,3 +13,6 @@ def authenticate():
     ee.Authenticate()
     ee.Initialize(project=config.GOOGLE_PROJECT_ID)
     print(ee.String("Hello from the Earth Engine servers!").getInfo())
+
+
+authenticate()
