fe2e8760da11129bf51ffaebe0ef2f802fbe134d
Jules Morley <julia.morley@code.berlin>
2024-10-29 10:50:58 +0100

    WIP: create basic component to test api connection

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,18 @@
 <template>
-  <h1>Hello world!</h1>
+  <h1 id="app">
+    <TestComponent />
+  </h1>
 </template>
 
-<script setup></script>
+<script>
+  import TestComponent from "./components/TestComponent.vue"
+
+  export default {
+    name: 'App',
+    components: {
+      TestComponent
+    }
+  };
+</script>
 
 <style scoped></style>
