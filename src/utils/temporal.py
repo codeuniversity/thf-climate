a5726a51799bd1fc84febc8622f402ab0f546aa0
tomADC443 <61840572+tomADC443@users.noreply.github.com>
2024-10-31 11:17:41 +0100

    fix_timeformat_UTC

--- src/utils/temporal.py
+++ src/utils/temporal.py
@@ -8,15 +8,30 @@ def get_optimistic_rounding(
 ) -> (datetime, datetime):
     """Get optimistic rounding for the start date based on the temporal resolution."""
     if temporal_resolution == TemporalResolution.DAILY:
-        start_date = start_date.replace(hour=0, minute=0, second=0, microsecond=0)
-        end_date = end_date.replace(hour=23, minute=59, second=59, microsecond=999999)
+        start_date = start_date.replace(
+            hour=0, minute=0, second=0, microsecond=0, tzinfo=datetime.timezone.utc
+        )
+        end_date = end_date.replace(
+            hour=23,
+            minute=59,
+            second=59,
+            microsecond=999999,
+            tzinfo=datetime.timezone.utc,
+        )
     elif temporal_resolution == TemporalResolution.MONTHLY:
         start_date = start_date.replace(
-            day=1, hour=0, minute=0, second=0, microsecond=0
+            day=1,
+            hour=0,
+            minute=0,
+            second=0,
+            microsecond=0,
+            tzinfo=datetime.timezone.utc,
         )
         end_date = last_time_of_month(end_date)
         # Set end_date to the last day of the month at 23:59:59.999999
-        next_month = end_date.replace(day=1, month=end_date.month % 12 + 1)
+        next_month = end_date.replace(
+            day=1, month=end_date.month % 12 + 1, tzinfo=datetime.timezone.utc
+        )
         end_date = next_month - timedelta(seconds=1)
     else:
         raise ValueError(f"Temporal resolution {temporal_resolution} not supported.")
@@ -26,9 +41,9 @@ def get_optimistic_rounding(
 def last_time_of_month(dt: datetime) -> datetime:
     # Move to the beginning of the next month
     if dt.month == 12:
-        next_month = datetime(dt.year + 1, 1, 1)
+        next_month = datetime(dt.year + 1, 1, 1, tzinfo=datetime.timezone.utc)
     else:
-        next_month = datetime(dt.year, dt.month + 1, 1)
+        next_month = datetime(dt.year, dt.month + 1, 1, tzinfo=datetime.timezone.utc)
 
     # Subtract one microsecond to get the last moment of the given month
     last_time = next_month - timedelta(microseconds=1)
