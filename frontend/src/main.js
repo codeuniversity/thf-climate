ee64d4edc0e1034ea52b31212c946ee996a72bfa
Jules Morley <julia.morley@code.berlin>
2024-11-04 17:07:52 +0100

    Update vuetify config

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,10 +3,15 @@ import App from './App.vue'
 
 import { createVuetify } from 'vuetify'
 import 'vuetify/styles'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 import '@mdi/font/css/materialdesignicons.css'
 
-const vuetify = createVuetify()
+const vuetify = createVuetify({
+  components,
+  directives,
+})
 
 createApp(App)
   .use(vuetify)
