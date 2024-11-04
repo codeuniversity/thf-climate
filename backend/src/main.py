610c6ca805ccfc48ede2123767eb821a7089a67e
Jules Morley <julia.morley@code.berlin>
2024-11-04 15:45:14 +0100

    Connect frontend to backend

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.routes.temp import weather_router
 
 app = FastAPI()
 
+origins = ["http://localhost:5173"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
