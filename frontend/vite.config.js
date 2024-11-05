e023ec2faa9ba040067e203c961a1ec602b3a40f
Till1983 <till_ermold@web.de>
2024-11-05 11:41:49 +0100

    remove proxy & restore previous setting

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,18 +2,15 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import 'dotenv/config'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    proxy: {
-      '/api': {
-        target: process.env.VITE_API_URL,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
