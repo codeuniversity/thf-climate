c584c0f45e11dbdb17bc92cf21de08cc6a5e7679
tomADC443 <61840572+tomADC443@users.noreply.github.com>
2024-11-06 13:25:59 +0100

    refactor: improve response time by processing on Gee Servers

--- src/gee/ndvi.py
+++ src/gee/ndvi.py
@@ -1,33 +1,67 @@
-from datetime import datetime
-
 import ee
 
-from src.gee.auth import authenticate
-from src.utils.temporal import get_start_of_day
+INDEX_FEATURE_LABEL = "indexing_value"
+TIMESTAMP_FEATURE_LABEL = "start_of_day_timestamp"
+INDEX_NULL_STRING = "NULL"
 
 
-def get_ndvi_info(masked_image_list: list[ee.Image], coordinates: ee.Geometry.Polygon):
-    authenticate()
+def calculate_mean_ndvi_GEE_SERVER(image: ee.Image, aoi: ee.Geometry.Polygon):
+    ndvi = image.normalizedDifference(["B8", "B4"]).rename("NDVI")
+    mean_ndvi = ndvi.reduceRegion(
+        reducer=ee.Reducer.mean(), geometry=aoi, scale=10, maxPixels=1e8
+    ).get("NDVI")
+    mean_ndvi = ee.Algorithms.If(
+        ee.Algorithms.IsEqual(mean_ndvi, None), INDEX_NULL_STRING, mean_ndvi
+    )
+    return image.set(INDEX_FEATURE_LABEL, mean_ndvi)
+
+
+def calculate_start_of_day_timestamp_GEE_SERVER(image: ee.Image):
+    acquisition_time = image.get("system:time_start")
+    start_of_day = (
+        ee.Date(acquisition_time)
+        .update(hour=0, minute=0, second=0)
+        .millis()
+        .divide(1000)
+    )
+    return image.set(TIMESTAMP_FEATURE_LABEL, start_of_day)
+
+
+def get_ndvi_info(
+    image_collection: ee.ImageCollection, coordinates: ee.Geometry.Polygon
+):
     aoi = ee.Geometry.Polygon(coordinates)
-    results = []
-    for masked_image in masked_image_list:
-        ndvi = masked_image.normalizedDifference(["B8", "B4"]).rename("NDVI")
 
-        # Calculate the mean NDVI within the AOI
-        mean_ndvi = (
-            ndvi.reduceRegion(
-                reducer=ee.Reducer.mean(),
-                geometry=aoi,
-                scale=10,  # Adjust scale based on Sentinel-2 resolution (10m)
-                maxPixels=1e8,
-            )
-            .get("NDVI")
-            .getInfo()
+    # Setting indexing values Server side
+    image_collection_with_ndvi = image_collection.map(
+        lambda img: calculate_mean_ndvi_GEE_SERVER(img, aoi)
+    )
+
+    # Setting timestamps Server side
+    image_collection_with_timestamp_and_ndvi = image_collection_with_ndvi.map(
+        lambda img: calculate_start_of_day_timestamp_GEE_SERVER(img)
+    )
+
+    #  Getting indexing values to the client side
+    index_value_list = image_collection_with_timestamp_and_ndvi.aggregate_array(
+        INDEX_FEATURE_LABEL
+    ).getInfo()
+
+    # Getting timestamps to the client
+    timestamp_list = image_collection_with_timestamp_and_ndvi.aggregate_array(
+        TIMESTAMP_FEATURE_LABEL
+    ).getInfo()
+
+    if len(timestamp_list) != len(index_value_list):
+        print(f"Timestamps: {len(timestamp_list)}, Values: {len(index_value_list)}")
+        raise ValueError(
+            "The lists of gee indexing values and timestamps do not have the same size"
         )
-        acquisition_time = masked_image.get("system:time_start").getInfo()
-        acquisition_datetime = datetime.utcfromtimestamp(acquisition_time / 1000)
-        acquisition_start_of_day = get_start_of_day(acquisition_datetime)
-        results.append({"timestamp": acquisition_start_of_day, "value": mean_ndvi})
 
-    print(f"Result length {len(results)}")
-    return results
+    # Convert the results to a list of dictionaries
+    combined_results = [
+        {"timestamp": ts, "value": val}
+        for ts, val in zip(timestamp_list, index_value_list)
+    ]
+
+    return combined_results
