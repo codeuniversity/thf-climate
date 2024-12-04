778db786a0e07590a42024f1973de990612c1410
Essam AbdelSabour <me@essam.ws>
2024-12-04 16:51:00 +0100

    create a route to display the env if exists

--- backend/src/weather/router.py
+++ backend/src/weather/router.py
@@ -1,9 +1,14 @@
 from fastapi import APIRouter, Depends
 from . import schemas, service
+import os
 
 router = APIRouter()
 
 @router.get("/index", response_model=schemas.WeatherDataResponse)
 async def get_weather_data(request: schemas.WeatherDataRequest = Depends()):
     return await service.fetch_weather_data(request)
+
+@router.get("/test-env")
+async def testenv():
+    return os.getenv("GOOGLE_EARTH_ENGINE_SERVICE_ACCOUNT_CREDENTIALS")
 
