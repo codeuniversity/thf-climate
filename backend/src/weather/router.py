ea118258169ddd6a3369722fa793eece082009a6
Essam AbdelSabour <me@essam.ws>
2024-12-04 17:13:53 +0100

    cleanup

--- backend/src/weather/router.py
+++ backend/src/weather/router.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter, Depends
 from . import schemas, service
-import os
 
 router = APIRouter()
 
 @router.get("/index", response_model=schemas.WeatherDataResponse)
 async def get_weather_data(request: schemas.WeatherDataRequest = Depends()):
     return await service.fetch_weather_data(request)
+
