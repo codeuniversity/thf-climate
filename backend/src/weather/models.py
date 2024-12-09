ddcac967a804d93f3b4055bdcc7ade4611a198ef
Essam AbdelSabour <me@essam.ws>
2024-12-09 13:49:06 +0100

    cache open meteo api data & refactor the code

--- backend/src/weather/models.py
+++ backend/src/weather/models.py
@@ -1,14 +1,20 @@
 from pydantic import BaseModel
+from abc import ABC, abstractmethod
 import requests
-from .utils import LocationLatLong
+from .utils import LocationLatLong, get_matching_json_file
 from datetime import datetime
 from ..constants import TemporalResolution
 from calendar import monthrange
 
-class WeatherData(BaseModel):
+class WeatherDataProvider(ABC):
+    @abstractmethod
+    def fetch_data(self, weatherVariable, temporalResolution, startDate, endDate, location):
+        pass
 
-    @staticmethod
-    def get(weatherVariable, temporalResolution, startDate, endDate, location):
+# Weather data class for getting the weather data from open meteo api
+class WeatherDataOpenMeteo(WeatherDataProvider):
+
+    def fetch_data(self, weatherVariable, temporalResolution, startDate, endDate, location):
 
         # format inputs
         if temporalResolution == TemporalResolution.DAILY:
@@ -18,7 +24,8 @@ class WeatherData(BaseModel):
             start_date = datetime.fromtimestamp(startDate).replace(day=1).strftime("%Y-%m-%d")
             dt = datetime.fromtimestamp(endDate)
             last_day = monthrange(dt.year, dt.month)[1]
-            end_date = dt.replace(day=last_day).strftime("%Y-%m-%d")
+            today = datetime.today().replace(hour=0, minute=0, second=0, microsecond=0)
+            end_date = min(dt.replace(day=last_day), today).strftime("%Y-%m-%d")
         coordinates = LocationLatLong[location].value
 
         # get the data from open-meteo api
@@ -33,3 +40,46 @@ class WeatherData(BaseModel):
 
         response = requests.get(url, params=params)
         return response.json()
+
+
+# Weather data class for getting the weather data from cached files
+class WeatherDataCached(WeatherDataProvider):
+
+    # get data
+    def fetch_data(self, weatherVariable, temporalResolution, startDate, endDate, location):
+
+        # get cached data
+        data = get_matching_json_file('src/weather/cached_data', startDate, endDate, weatherVariable.value)
+
+        # change the provider to open meteo if there is no cached data
+        if data == None:
+            provider = WeatherDataOpenMeteo()
+            return provider.fetch_data(weatherVariable, temporalResolution, startDate, endDate, location)
+
+        return self.filter_data(data, weatherVariable, temporalResolution, startDate, endDate)
+
+    # filter the cached data
+    def filter_data(self, data, weatherVariable, temporalResolution, startDate, endDate):
+
+        # format date
+        if temporalResolution == TemporalResolution.DAILY:
+            start_date = datetime.fromtimestamp(startDate)
+            end_date = datetime.fromtimestamp(endDate)
+        elif temporalResolution == TemporalResolution.MONTHLY:
+            start_date = datetime.fromtimestamp(startDate).replace(day=1)
+            dt = datetime.fromtimestamp(endDate)
+            last_day = monthrange(dt.year, dt.month)[1]
+            end_date = dt.replace(day=last_day)
+
+        # format data
+        time_data = data["hourly"]["time"]
+        weather_data = data["hourly"][weatherVariable.value]
+        filtered_data = filter(
+            lambda item: start_date <= datetime.strptime(item[0], "%Y-%m-%dT%H:%M") <= end_date,
+            zip(time_data, weather_data),
+        )
+        filtered_time, filtered_weather = zip(*filtered_data)
+        data["hourly"]["time"] = list(filtered_time)
+        data["hourly"][weatherVariable.value] = list(filtered_weather)
+
+        return data
--- backend/src/weather/utils.py
+++ backend/src/weather/utils.py
@@ -1,4 +1,42 @@
 from enum import Enum
+from fastapi import HTTPException
+import time
+import os
+import json
 
 class LocationLatLong(Enum):
     TEMPELHOFER_FELD = (52.4906898,13.373749)
+
+def validate_timestamp_in_range(start, end):
+    min_timestamp = -946771200  # 01/01/1940
+    max_timestamp = int(time.time()) # now
+    if (start < min_timestamp or end > max_timestamp):
+        raise HTTPException(
+            status_code=400, detail=f"Timestamp must be between {min_timestamp} and {max_timestamp}")
+    elif end <= start:
+        raise HTTPException(
+            status_code=400, detail="endDate must be after startDate")
+    return end
+
+# Get json file from folder based on the timestamp
+# matching format <start_timestamp>_<end_timestamp>_<file_name>.json
+def get_matching_json_file(folder_path, timestamp_from, timestamp_to, filename):
+    for file_name in os.listdir(folder_path):
+        if file_name.endswith(f'_{filename}.json'):
+            try:
+                # Extract start and end timestamps from the filename
+                parts = file_name.split('_')
+                file_start_timestamp = int(parts[0])
+                file_end_timestamp = int(parts[1])
+
+                # Check if the provided timestamps fall within the file's range
+                if file_start_timestamp <= timestamp_from and file_end_timestamp >= timestamp_to:
+                    file_path = os.path.join(folder_path, file_name)
+
+                    # Load and return the JSON content
+                    with open(file_path, 'r') as json_file:
+                        return json.load(json_file)
+            except (ValueError, IndexError, FileNotFoundError):
+                continue
+
+    return None
