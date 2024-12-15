2e0acfefece857a9301e4870c29408b379745928
Jules Morley <julia.morley@code.berlin>
2024-12-15 16:01:33 +0100

    Add vitest config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,4 +11,11 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    transformMode: {
+      web: [/\.vue$/],
+    },
+    }
 })
